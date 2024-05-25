69f6eefc3366f1edc5b186b5763583348e100470
Mahesh-2007 <mm2472229@gmail.com>
2024-05-25 14:30:59 +0530

    alert added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var input = document.getElementById("input");
 var list = document.getElementById("list");
 function add() {
      if (input.value.trim() === '') {
+        alert("Please enter a task")
 return;}
     var li = document.createElement("li");
     li.innerHTML = "⭐ㅤ"+ input.value +"ㅤㅤ"+ "<button class='dbutton' onclick='Delete(event)'>Delete</button>";
