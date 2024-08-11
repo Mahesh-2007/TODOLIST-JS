4a2750be788c53605b29813553f1bc8ffd206982
Mahesh-2007 <mm2472229@gmail.com>
2024-08-11 18:41:21 +0530

    screen adjust

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 var input = document.getElementById("input");
 var list = document.getElementById("list");
+
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        add();
+    }
+});
+
 function add() {
-     if (input.value.trim() === '') {
-        alert("Please enter a task")
-return;}
+    if (input.value.trim() === '') {
+        alert("Please enter a task");
+        return;
+    }
     var li = document.createElement("li");
-    li.innerHTML = "⭐ㅤ"+ input.value +"ㅤㅤ"+ "<button class='dbutton' onclick='Delete(event)'>Delete</button>";
+    li.innerHTML = "⭐ㅤ" + input.value + "ㅤㅤ" + "<button class='dbutton' onclick='Delete(event)'>Delete</button>";
     list.appendChild(li);
     input.value = "";
 }
-function Delete(event)
-{
-   event.target.parentElement.remove();
+
+function Delete(event) {
+    event.target.parentElement.remove();
 }
